2f86dce82be3b5f7f9c10dd3d67d13bd0c061bcd
Viraj Turakhia <virajturakhia@gmail.com>
2011-03-14 20:44:08 -0700

    pause/resume feature

--- src/com/gebogebo/android/distancecalcfree/DistanceCalculatorService.java
+++ src/com/gebogebo/android/distancecalcfree/DistanceCalculatorService.java
@@ -20,27 +20,34 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class DistanceCalculatorService extends Service implements LocationListener {
-    private final Set<DistanceCalculatorServiceListener> LISTENERS = 
-        new HashSet<DistanceCalculatorServiceListener>();
-    
-    private static final int MIN_ACCURACY = 50; //minimum accuracy in meters
+    private enum SERVICE_MODE {
+        started, stopped, paused, resumed
+    };
+
+    private final Set<DistanceCalculatorServiceListener> LISTENERS = new HashSet<DistanceCalculatorServiceListener>();
+
+    private static final int MIN_ACCURACY = 50; // minimum accuracy in meters
     private Location prevLocation = null;
     private float actualDistanceCovered = -1.0F;
     private long startTimeInSecs = -1L;
+    private SERVICE_MODE currentServiceMode = SERVICE_MODE.stopped;
+    private String action = null;
+    private long totalPausedTimeInMillis = 0l;
+    private long pausedStartTimeInMillis = 0l;
     private final DistanceServiceBinder binder = new DistanceServiceBinder();
-    
+
     /**
      * adds listener to the service
-     *  
+     * 
      * @param listener listener to be added
      */
     public void addListener(DistanceCalculatorServiceListener listener) {
         LISTENERS.add(listener);
-        if(actualDistanceCovered > 0.0) {
+        if (actualDistanceCovered > 0.0) {
             updateListenersWithDistance(null);
         }
     }
-    
+
     /**
      * removes listener from the list of interested parties
      * 
@@ -49,82 +56,152 @@ public class DistanceCalculatorService extends Service implements LocationListen
     public void removeListener(DistanceCalculatorServiceListener listener) {
         LISTENERS.remove(listener);
     }
-    
+
     /**
      * start this service
      */
     public void start() {
         goForeground();
-        startTimeInSecs = System.currentTimeMillis() / 1000;
-        startTimeInSecs--; //decrement time so that 0 is never returned as time elapsed
-        SharedPreferences defPref = PreferenceManager.getDefaultSharedPreferences(this);
-        //default selection is miles
-        String action = defPref.getString(DistanceCalculatorPrefActivity.PREF_KEY_ACTION, 
-        		DistanceCalculatorPrefActivity.PREF_BIKING);
-        Integer gpsFreqInMillis = DistanceCalculatorPrefActivity.getFrequencyInMillisForAction(action);
-        Integer gpsFreqInDistance = DistanceCalculatorPrefActivity.getFrequencyInMetersForAction(action);
-        Log.i("service", "action " + action + ": distance=" + gpsFreqInDistance + " ,freq=" + gpsFreqInMillis);
-        LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, gpsFreqInMillis, gpsFreqInDistance, this);
-        Log.i("service", "Started listening to location changes");
+        setServiceParameters(SERVICE_MODE.started);
+        startListeningToLocationEvents();
     }
-    
+
     /**
      * stop this service
      */
     public void stop() {
-        LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        if (SERVICE_MODE.paused.equals(currentServiceMode)) {
+            resume();
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationManager.removeUpdates(this);
         stopForeground(true);
-        prevLocation = null;
-        actualDistanceCovered = -1.0F;
-        startTimeInSecs = -1L;
+        setServiceParameters(SERVICE_MODE.stopped);
         Log.i("service", "Stopped listening to location changes");
     }
-    
+
+    /**
+     * called when service is to be paused. service stops listening to location events and also exclude this time from
+     * total time spent by app
+     */
+    public void pause() {
+        if (SERVICE_MODE.paused.equals(currentServiceMode)) {
+            return; // already paused. nothing to do
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.removeUpdates(this);
+        setServiceParameters(SERVICE_MODE.paused);
+    }
+
+    /**
+     * resumes service's functionality from paused mode. if service is not paused when this is called, no action is
+     * taken
+     */
+    public void resume() {
+        if (!SERVICE_MODE.paused.equals(currentServiceMode)) {
+            return; // nothing to do here as service is not in paused mode
+        }
+        startListeningToLocationEvents();
+        setServiceParameters(SERVICE_MODE.resumed);
+    }
+
     /**
      * returns distance covered so far (since service was start using {@link #start()})
+     * 
      * @return
      */
     public float getCurrentDistance() {
         return actualDistanceCovered;
     }
-    
+
     /**
      * returns number of seconds elapsed since service was started
+     * 
      * @return
      */
     public long getTimeElapsed() {
-    	return System.currentTimeMillis()/1000 - startTimeInSecs;
+        return (System.currentTimeMillis() - totalPausedTimeInMillis) / 1000 - startTimeInSecs;
     }
-    
+
+    /**
+     * sets service parameters as per current mode in which service is running
+     * 
+     * @param mode service mode in which service is currently running in
+     */
+    private void setServiceParameters(SERVICE_MODE mode) {
+        switch (mode) {
+            case started:
+                prevLocation = null;
+                actualDistanceCovered = -1.0F;
+                totalPausedTimeInMillis = 0l;
+                pausedStartTimeInMillis = 0l;
+                startTimeInSecs = System.currentTimeMillis() / 1000;
+                startTimeInSecs--; // decrement time so that 0 is never returned as time elapsed
+                SharedPreferences defPref = PreferenceManager.getDefaultSharedPreferences(this);
+                action = defPref.getString(DistanceCalculatorPrefActivity.PREF_KEY_ACTION,
+                        DistanceCalculatorPrefActivity.PREF_BIKING);
+                break;
+            case paused:
+                prevLocation = null;
+                pausedStartTimeInMillis = System.currentTimeMillis();
+                break;
+            case resumed:
+                totalPausedTimeInMillis += System.currentTimeMillis() - pausedStartTimeInMillis;
+                pausedStartTimeInMillis = 0l;
+                break;
+            case stopped:
+                prevLocation = null;
+                actualDistanceCovered = -1.0F;
+                startTimeInSecs = -1L;
+                totalPausedTimeInMillis = 0l;
+                action = null;
+                break;
+        }
+        Log.i("service", "mode changed to " + mode);
+        currentServiceMode = mode;
+    }
+
+    /**
+     * starts listening to location events from locationmanager. location events parameters are obtained from the
+     * parameters which are set by user
+     */
+    private void startListeningToLocationEvents() {
+        Integer gpsFreqInMillis = DistanceCalculatorPrefActivity.getFrequencyInMillisForAction(action);
+        Integer gpsFreqInDistance = DistanceCalculatorPrefActivity.getFrequencyInMetersForAction(action);
+        Log.i("service", "action " + action + ": distance=" + gpsFreqInDistance + " ,freq=" + gpsFreqInMillis);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, gpsFreqInMillis, gpsFreqInDistance, this);
+        Log.i("service", "Started listening to location changes");
+    }
+
     /**
      * updates all interested parties with change in distance
      * 
      * @param latestLocation latest location recorded
      */
     private void updateListenersWithDistance(Location latestLocation) {
-        for(DistanceCalculatorServiceListener listener : LISTENERS) {
+        for (DistanceCalculatorServiceListener listener : LISTENERS) {
             listener.onDistanceChange(actualDistanceCovered, getTimeElapsed(), latestLocation);
         }
     }
-    
+
     /**
      * updates all interested parties if there was any change in service's availability
-     *  
+     * 
      * @param errorCode error code, if any, resulting from change in status
      */
     private void updateListenersWithServiceAvailability(int errorCode) {
-        for(DistanceCalculatorServiceListener listener : LISTENERS) {
+        for (DistanceCalculatorServiceListener listener : LISTENERS) {
             listener.onServiceStatusChange(errorCode);
         }
     }
-    
+
     /**
      * bring service to foreground. this prevent service/app from being killed by android's task manager
      */
     private void goForeground() {
-        Notification notification = new Notification(R.drawable.ruler_status, getString(R.string.notificationTitle), System.currentTimeMillis());
+        Notification notification = new Notification(R.drawable.ruler_status, getString(R.string.notificationTitle),
+                System.currentTimeMillis());
         Context context = getApplicationContext();
         CharSequence contentTitle = getString(R.string.notificationTitle);
         CharSequence contentText = getString(R.string.notificationMsg);
@@ -135,24 +212,34 @@ public class DistanceCalculatorService extends Service implements LocationListen
         startForeground(1, notification);
         Log.i("service", "service brought to foreground");
     }
-    
-    //location listener overrides
+
+    // location listener overrides
     @Override
     public void onLocationChanged(Location newLocation) {
-        if(newLocation.getAccuracy() > MIN_ACCURACY) {
-        	Log.i("service", "accuracy too low: " + newLocation.getAccuracy());
-        	return;
+        if (SERVICE_MODE.paused.equals(currentServiceMode) || SERVICE_MODE.stopped.equals(currentServiceMode)) {
+            Log.i("service", "paused or stopped mode.. nothing to update");
+            // if service is in paused state, ignore these msgs
+            return;
         }
-        if(prevLocation != null) {
+        if (newLocation.getAccuracy() > MIN_ACCURACY) {
+            Log.i("service", "accuracy too low: " + newLocation.getAccuracy());
+            return;
+        }
+        // reaches here only if current mode is started or resumed
+        if (prevLocation != null) {
+            //if previous location is not null, it doesn't matter if service was resumed or started, we calculate distance
+            Log.i("service", "prev location is not null");
+            // for both.. started and resumed
             float distance = newLocation.distanceTo(prevLocation);
-            prevLocation = newLocation;
             actualDistanceCovered += distance;
-            updateListenersWithDistance(newLocation);
-        } else {
-            prevLocation = newLocation;
+        } else if (SERVICE_MODE.started.equals(currentServiceMode)) {
+            //if previous location is null and if mode is started, only then we will reset actualDistanceCovered
+            //if it was resumed, we need not set actualDistanceCovered
+            Log.i("service", "prev location is null and service is just started");
             actualDistanceCovered = 0.0F;
-            updateListenersWithDistance(newLocation);
         }
+        prevLocation = newLocation;
+        updateListenersWithDistance(newLocation);
     }
 
     @Override
@@ -167,38 +254,38 @@ public class DistanceCalculatorService extends Service implements LocationListen
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        if(provider.equals(LocationManager.GPS_PROVIDER)) {
+        if (provider.equals(LocationManager.GPS_PROVIDER)) {
             Log.d("service", "GPS service status change");
             updateListenersWithServiceAvailability(status);
         }
     }
-    
-    //Service lifecycle methods
+
+    // Service lifecycle methods
     @Override
     public void onCreate() {
         Log.i("service", "Service created");
         super.onCreate();
     }
-    
+
     @Override
     public int onStartCommand(Intent i, int flags, int startId) {
         Log.i("service", "On start command called");
         super.onStartCommand(i, flags, startId);
         return Service.START_NOT_STICKY;
     }
-    
+
     @Override
     public void onDestroy() {
         Log.i("service", "Service is being destroyed");
         stop();
         super.onDestroy();
     }
-    
+
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
     }
-    
+
     class DistanceServiceBinder extends Binder {
         DistanceCalculatorService getService() {
             return DistanceCalculatorService.this;
--- src/com/gebogebo/android/distancecalcfree/DistanceCalculatorUtilities.java
+++ src/com/gebogebo/android/distancecalcfree/DistanceCalculatorUtilities.java
@@ -10,31 +10,49 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.location.LocationProvider;
 import android.os.Environment;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class DistanceCalculatorUtilities {
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy.MMM.DD_HH.mm.ss");
-	private static final String SCREENSHOT_DIR = "distancecalc";
-	
-	/**
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy.MMM.DD_HH.mm.ss");
+    private static final String SCREENSHOT_DIR = "distancecalc";
+
+    /**
      * converts given float distance and returns string representation which is directly displayable on activity
      * 
      * @param newDistanceInMeters float distance to be converted (in meters)
      * @param multiplier distance unit multiplier. 1.00 for km, 0.6xx for miles
-     * @param distanceSuffix distance suffix to be used based on distance unit 
+     * @param distanceSuffix distance suffix to be used based on distance unit
      * @return
      */
-    public String getVisualDistance(float newDistanceInMeters, long totalTimeInSecs, float multiplier, 
-    		String distanceSuffix, String hourStr) {
-//        Log.i("util", "visual mult: " + multiplier + " dist: " + distanceSuffix);
-        int viewingDistance = (int)(newDistanceInMeters * multiplier);
-        //speed is always calculated per hour basis
-        float speed = (float)(newDistanceInMeters * 3.6 * multiplier) / totalTimeInSecs; //3.6 = 3600 min / 1000 (for meters to km)
-        return String.format("%.3f %s at %.2f %s/%s", (float)viewingDistance/1000, distanceSuffix, speed, distanceSuffix, hourStr);
+    public String getVisualDistance(float newDistanceInMeters, long totalTimeInSecs, float multiplier,
+            String distanceSuffix, String hourStr) {
+        // Log.i("util", "visual mult: " + multiplier + " dist: " + distanceSuffix);
+        int viewingDistance = (int) (newDistanceInMeters * multiplier);
+        // speed is always calculated per hour basis
+        float speed = (float) (newDistanceInMeters * 3.6 * multiplier) / totalTimeInSecs; // 3.6 = 3600 min / 1000 (for
+                                                                                          // meters to km)
+        return String.format("%.3f %s at %.2f %s/%s", (float) viewingDistance / 1000, distanceSuffix, speed,
+                distanceSuffix, hourStr);
     }
-    
+
+    /**
+     * returns visual representable elapsed time
+     * 
+     * @param timeElapsed number of seconds which are to be converted to formatted
+     * @param timeElapsedFormattedString format string which holds format in which time is to be formatted
+     * 
+     * @return formatted string representation of passed number of seconds
+     */
+    public String getVisualTime(long timeElapsed, String timeElapsedFormattedString) {
+        if (timeElapsed < 0) {
+            timeElapsed = 0;
+        }
+        return String.format(timeElapsedFormattedString, DateUtils.formatElapsedTime(timeElapsed));
+    }
+
     /**
      * captures screenshot for given view object
      * 
@@ -42,55 +60,54 @@ public class DistanceCalculatorUtilities {
      * @param c context from which view is selected
      */
     public void saveParentView(View v, Context c) {
-    	v.setDrawingCacheEnabled(true);
-    	Bitmap bitmap = v.getDrawingCache();
-    	try {
-    		if(!Environment.MEDIA_MOUNTED.equalsIgnoreCase(Environment.getExternalStorageState())) {
-    			Log.i("activity", "external storage not mounted");
-    			Toast.makeText(c, R.string.msg_no_storage, Toast.LENGTH_LONG).show();
-    			//inform user that external storage device is not mounted
-    			return;
-    		}
-    		File dirPath = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + SCREENSHOT_DIR
-    				+ File.separator);
-    		if(! dirPath.exists()) {
-    			if(!dirPath.mkdir()) {
-    				//let user know about error
-    				Log.i("activity", "unable to create directory");
-    				return;
-    			}
-    		}
-    		Date now = new Date();
-    		String filename = dirPath + File.separator + DATE_FORMAT.format(now) + ".png";
-    		//File dirPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-    		Log.i("activity", "external storage dir: " + filename);
-    		File newFile = new File(filename);
-    		FileOutputStream outStream = new FileOutputStream(newFile);
-    		bitmap.compress(CompressFormat.PNG, 90, outStream);
-    		Log.i("activity", "screenshot captured");
-    		Toast.makeText(c, String.format(c.getString(R.string.msg_file_saved), filename), Toast.LENGTH_LONG).show();
-    	} catch(Exception e) {
-    		Log.e("activity", "unable to capture screenshot: " + e);
-    		Toast.makeText(c, R.string.msg_unable_to_save, Toast.LENGTH_LONG).show();
-    	}
+        v.setDrawingCacheEnabled(true);
+        Bitmap bitmap = v.getDrawingCache();
+        try {
+            if (!Environment.MEDIA_MOUNTED.equalsIgnoreCase(Environment.getExternalStorageState())) {
+                Log.i("activity", "external storage not mounted");
+                Toast.makeText(c, R.string.msg_no_storage, Toast.LENGTH_LONG).show();
+                // inform user that external storage device is not mounted
+                return;
+            }
+            File dirPath = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator
+                    + SCREENSHOT_DIR + File.separator);
+            if (!dirPath.exists()) {
+                if (!dirPath.mkdir()) {
+                    // let user know about error
+                    Log.i("activity", "unable to create directory");
+                    return;
+                }
+            }
+            Date now = new Date();
+            String filename = dirPath + File.separator + DATE_FORMAT.format(now) + ".png";
+            Log.i("activity", "external storage dir: " + filename);
+            File newFile = new File(filename);
+            FileOutputStream outStream = new FileOutputStream(newFile);
+            bitmap.compress(CompressFormat.PNG, 90, outStream);
+            Log.i("activity", "screenshot captured");
+            Toast.makeText(c, String.format(c.getString(R.string.msg_file_saved), filename), Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Log.e("activity", "unable to capture screenshot: " + e);
+            Toast.makeText(c, R.string.msg_unable_to_save, Toast.LENGTH_LONG).show();
+        }
     }
-    
+
     /**
      * obtains formatted string which can be displayed directly to show current speed
      * 
      * @param speedInMetersPerSec current speed in m/s
      * @param multiplier multiplier based on distance unit selected by user
      * @param distanceSuffix suffix to use based on distance unit selected by user
-     * @param hourStr string to be used to display hours string 
-     * @param formattedString format string to be used for to display current speed 
+     * @param hourStr string to be used to display hours string
+     * @param formattedString format string to be used for to display current speed
      * @return formatted string with substitutions which can be displayed directly to show current speed
      */
-    public String getVisualCurrentSpeed(float speedInMetersPerSec, float multiplier, String distanceSuffix, 
-    		String hourStr, String formattedString) {
-    	float speed = (float)(speedInMetersPerSec * 3.6 * multiplier);
-    	return String.format(formattedString, speed, distanceSuffix, hourStr);
+    public String getVisualCurrentSpeed(float speedInMetersPerSec, float multiplier, String distanceSuffix,
+            String hourStr, String formattedString) {
+        float speed = (float) (speedInMetersPerSec * 3.6 * multiplier);
+        return String.format(formattedString, speed, distanceSuffix, hourStr);
     }
-    
+
     /**
      * returns corresponding error code id from string resources, which corresponds to passed errorCode
      * 
@@ -98,7 +115,7 @@ public class DistanceCalculatorUtilities {
      * @return string resources id for given error code
      */
     public int getErrorTextId(int errorCode) {
-        if(LocationProvider.OUT_OF_SERVICE == errorCode) {
+        if (LocationProvider.OUT_OF_SERVICE == errorCode) {
             Log.d("locationService", "GPS service not available");
             return R.string.service_not_available;
         } else if (LocationProvider.TEMPORARILY_UNAVAILABLE == errorCode) {
